656646ea3ecc8cc5dfce18a105f9749693c99bc2
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2024-12-12 12:28:19 +0500

    some changes

--- jspart2.js
+++ jspart2.js
@@ -1,5 +1,10 @@
+/**
+ *
+ * @param {list} array of numerical values
+ * @returns {float} mean of the array
+ */
 function mean_of_arr(array){
-    sum_of_array = 0
+    let sum_of_array = 0
     for (let i=0; i < array.length; i++) {
         sum_of_array = array[i] + sum_of_array
     }
@@ -7,5 +12,5 @@ function mean_of_arr(array){
     return {sum_of_array, mean_of_array}
 }
 
-arr = [3,4,5,6]
+const arr = [3,4,5,6]
 console.log(mean_of_arr(arr))
