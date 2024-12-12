d351afc7547e49b96eddcb72b6cbaea53c4d6eea
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2024-12-12 12:10:06 +0500

    error handling

--- index.js
+++ index.js
@@ -6,12 +6,15 @@
 
 
 function quadratic_equ(a, b, c) {
-    underroot_term = Math.sqrt(b*b - 4*a*c)
+    underroot_term = b*b - 4*a*c
+    if (underroot_term < 0) {
+        return { error : 'no real roots'}
+    }
     divide_term =2*a
     add_term = -b
-    x1 =(add_term + underroot_term)/(divide_term)
-    x2 =(add_term - underroot_term)/(divide_term)
-    return console.log(x1, x2)
+    x1 =(add_term + Math.sqrt(underroot_term))/(divide_term)
+    x2 =(add_term - Math.sqrt(underroot_term))/(divide_term)
+    return { x1, x2}
 }
 
-quadratic_equ(1, 4, 3)
+console.log(quadratic_equ(1, 4, 3))
