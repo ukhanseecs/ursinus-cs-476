d73cec5bc0ebe34ad22bd3c454e2e0567f6bd8ac
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2024-12-12 14:57:01 +0500

    angle computation success

--- HW1_GeomPrimitives-master/geomprimitives.js
+++ HW1_GeomPrimitives-master/geomprimitives.js
@@ -12,11 +12,26 @@ vec3 = glMatrix.vec3;
  * @param {vec3} a First point
  * @param {vec3} b Second point
  * @param {vec3} c Third point
- * 
+ *
  * @return {float} Angle between vectors ab and ac in degrees
  */
 function getAngle(a, b, c) {
     // TODO: Fill this in
+	let ab = vec3.create()
+	let ac = vec3.create()
+
+	vec3.subtract(ab, b, a)
+	vec3.subtract(ac, c, a)
+
+	let dot_prod = vec3.dot(ab, ac)
+	let mag_ab = vec3.length(ab)
+	let mag_ac = vec3.length(ac)
+
+	let theta = Math.acos(dot_prod/(mag_ab*mag_ac))
+	if (mag_ab == 0 || mag_ac == 0){
+		return -1
+	}
+	return theta = theta * (180/Math.PI)
 }
 
 
@@ -25,19 +40,20 @@ function getAngle(a, b, c) {
  * Project vector u onto vector v using the glMatrix library
  * @param {vec3} u Vector that's being projected
  * @param {vec3} v Vector onto which u is projected
- * 
+ *
  * @return {vec3} The projection of u onto v
  */
 function projVector(u, v) {
-    // TODO: Fill this in
+    // TODO: Fill this
+
 }
 
 
 /**
- * 
+ *
  * @param {vec3} u Vector that's being projected
  * @param {vec3} v Vector onto which u is perpendicularly projected
- * 
+ *
  * @return {vec3} The perpendicular projection of u onto v
  */
 function projPerpVector(u, v) {
@@ -46,12 +62,12 @@ function projPerpVector(u, v) {
 
 
 /**
- * Given three 3D vertices a, b, and c, compute the area 
+ * Given three 3D vertices a, b, and c, compute the area
  * of the triangle they span
  * @param {vec3} a First point
  * @param {vec3} b Second point
  * @param {vec3} c Third point
- * 
+ *
  * @return {float} Area of the triangle
  */
 function getTriangleArea(a, b, c) {
@@ -61,13 +77,13 @@ function getTriangleArea(a, b, c) {
 
 /**
  * For a plane determined by the points a, b, and c, with the plane
- * normal determined by those points in counter-clockwise order using 
+ * normal determined by those points in counter-clockwise order using
  * the right hand rule, decide whether the point d is above, below, or on the plane
  * @param {vec3} a First point on plane
  * @param {vec3} b Second point on plane
  * @param {vec3} c Third point on plane
  * @param {vec} d Test point
- * 
+ *
  * @return {int} 1 if d is above, -1 if d is below, 0 if d is on
  */
 function getAboveOrBelow(a, b, c, d) {
@@ -89,12 +105,12 @@ function getAboveOrBelow(a, b, c, d) {
 
 /**
  * Compute the barycentric coordinates of a point p with respect to a triangle /\abc
- * 
+ *
  * @param {vec3} a Point a on the triangle
  * @param {vec3} b Point b on the triangle
  * @param {vec3} c Point c on the triangle
  * @param {vec3} p The point whose barycentric coordinates we seek
- * 
+ *
  * @return {vec3} An vec3 with the barycentric coordinates (alpha, beta, gamma)
  * 				  corresponding to a, b, and c, respectively, so that
  * 				  alpha + beta + gamma = 1, and alpha, beta, gamma >= 0
@@ -111,18 +127,18 @@ function getBarycentricCoords(a, b, c, p) {
 
 /**
  * Find the intersection of a ray with a triangle
- * 
- * @param {vec3} p0 Endpoint of ray 
+ *
+ * @param {vec3} p0 Endpoint of ray
  * @param {vec3} v Direction of ray
  * @param {vec3} a Triangle vertex 1
  * @param {vec3} b Triangle vertex 2
  * @param {vec3} c Triangle vertex 3
- * 
+ *
  * @return {list} A list of vec3 objects.  The list should be empty
- *          if there is no intersection, or it should contain 
+ *          if there is no intersection, or it should contain
  *          exactly one vec3 object if there is an intersection
  *          CORNER CASES:
- *          (1) If the ray is parallel to the plane, 
+ *          (1) If the ray is parallel to the plane,
 *               return an empty list
  */
 function rayIntersectTriangle(p0, v, a, b, c) {
@@ -134,13 +150,13 @@ function rayIntersectTriangle(p0, v, a, b, c) {
 /**
  * Find the intersection of the ray p0 + tv, t >= 0, with the
  * sphere centered at c with radius r.
- * 
+ *
  * @param {vec3} p0 Endpoint of the ray
  * @param {vec3} v Direction of the ray
  * @param {vec3} c Center of the sphere
  * @param {number} r Radius of the sphere
- * 
- * @return {list of vec3} A list of intersection points, 
+ *
+ * @return {list of vec3} A list of intersection points,
  *   ***in the order in which the ray hits them***
  * If the ray doesn't hit any points, this list should be empty.
  * Note that a ray can hit at most 2 points on a sphere.
